eecff6fab496ad31e18391d984166212b542b4f9
Naitik Chattaraj <naitikchattaraj41@gmail.com>
2025-07-15 21:22:33 +0530

    hero

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,10 +11,40 @@ const Home = () => {
 
   return (
     <div id="main">
-      <section>
-      <h1>Home</h1>
-      <spline-viewer className="spline" url="https://prod.spline.design/6SMhyKFA1PgFr9vC/scene.splinecode"></spline-viewer>
-      </section>
+      <section className="section-1">
+  <div className="bg-video">
+<spline-viewer className="spline" url="https://prod.spline.design/6SMhyKFA1PgFr9vC/scene.splinecode"></spline-viewer>
+    <div className="video-overlay"></div>
+  </div>
+
+  <div className="section-content">
+    <h2>SOUND BEYOND <br />SILENCE</h2>
+    <p>
+      Immerse yourself into the beats of Auraluxe.
+    </p>
+    <div className="buttons">
+      <a href="#section-2" class="btn btn-primary">SHOP NOW</a>
+      <a
+        href="http://localhost:3000"
+        className="btn btn-secondary"
+      >
+        <span>
+          Learn More&nbsp;
+          <img
+            className="arrow arrow-default"
+            src="src/assets/rightward-arrow.png"
+            alt="→"
+          />
+          <img
+            className="arrow arrow-hover"
+            src="src/assets/rightward-arrow-black.png"
+            alt="→"
+          />
+        </span>
+      </a>
+    </div>
+  </div>
+</section> 
     </div>
   );
 };
