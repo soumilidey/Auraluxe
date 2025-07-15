32b8ce3bb60d30b2b69b50047dfdc7d8ce23af44
Naitik Chattaraj <naitikchattaraj41@gmail.com>
2025-07-15 20:41:27 +0530

    spline added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,22 @@
+import React, { useEffect } from "react";
 import "/src/components/Home.css";
-import React from "react";
 
 const Home = () => {
-  return <div id="main">Home</div>;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = "https://unpkg.com/@splinetool/viewer@1.10.31/build/spline-viewer.js";
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div id="main">
+      <section>
+      <h1>Home</h1>
+      <spline-viewer className="spline" url="https://prod.spline.design/6SMhyKFA1PgFr9vC/scene.splinecode"></spline-viewer>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
